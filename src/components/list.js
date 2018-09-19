a14e190dec90b50195e0ed743a3133104caf6f4f
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-09-19 14:59:43 -0700

    completed dummy data  hank kim

---
+++ src/components/list.js
@@ -0,0 +1,42 @@
+import React , { Component } from 'react';
+import dummyListData from '../dummy_data/list_data';
+
+// console.log('Dummydata',dummyListData);
+
+class List extends Component{
+    state = {
+        list: []
+    }
+
+    componentDidMount(){
+        this.getListData();
+    }
+
+    getListData(){
+        // Call sever to get data
+
+        this.setState({
+            list: dummyListData
+        })
+    }
+
+    render(){
+        // console.log('State', this.state);
+        const listElements = this.state.list.map((item, index) =>{
+            return <li className="collection-item" key={item._id}>{item.title}</li>
+        });
+
+
+
+        return(
+              <div>
+                {/* <h1> List goes here</h1> */}
+                <ul className="collection">
+                    {listElements}
+                </ul>
+             </div>
+        );
+    }
+}
+
+export default List;
---
+++ src/dummy_data/list_data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title : 'Go to store',
+        details: 'Get eggs, milk, bread, and diet coke',
+        _id: '001'
+    },
+    {
+        title : 'Go to gym',
+        details: 'go to practice and get health',
+        _id: '002'
+    },
+    {
+        title : 'Go to home',
+        details: 'Get sleep and eat',
+        _id: '003'
+    },
+    {
+        title : 'Go to park',
+        details: 'play baseball and soccer',
+        _id: '004'
+    }
+];
